f281a64e3bf2c88d0f3c8c4c717a73519a332f80
makewear <v.chupovsky@makewear.com.ua>
2015-09-21 19:01:11 +0300

    del sett

--- verify/settings/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-  mysql_connect("217.12.201.205", "zoond_make_r", "makewear12") or die ("Ошибка соединения с базой, проверьте настройки".mysql_error());
-  mysql_select_db("zoond_make");
- mysql_query("SET NAMES UTF8");
-?>
